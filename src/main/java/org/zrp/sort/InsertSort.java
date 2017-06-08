35d0abebaa7d97959cb1b770ebe53971199469d8
zhangrp <xk618268!-3>
2017-06-08 18:54:37 +0800

    zhangruiping

---
+++ src/main/java/org/zrp/sort/InsertSort.java
@@ -0,0 +1,12 @@
+/**
+ * $Id: InsertSort.java,v 1.0 2017/6/8 обнГ6:54 zhangruiping Exp $
+ */
+package org.zrp.sort;
+
+/**
+ * @author zhangruiping
+ * @version $Id: InsertSort.java,v 1.1 2017/6/8 обнГ6:54 zhangruiping Exp $
+ *          Created on 2017/6/8 обнГ6:54
+ */
+public class InsertSort {
+}
---
+++ src/main/java/org/zrp/sort/QuickSort.java
@@ -0,0 +1,12 @@
+/**
+ * $Id: QuickSort.java,v 1.0 2017/6/8 обнГ6:52 zhangruiping Exp $
+ */
+package org.zrp.sort;
+
+/**
+ * @author zhangruiping
+ * @version $Id: QuickSort.java,v 1.1 2017/6/8 обнГ6:52 zhangruiping Exp $
+ *          Created on 2017/6/8 обнГ6:52
+ */
+public class QuickSort {
+}
